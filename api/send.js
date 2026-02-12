7fd2e6d05f6f3332779cecf98328bef39c5d7d9c
Yousef Shaaban <101521680+YousefShaabanSaad@users.noreply.github.com>
2026-02-13 01:20:57 +0200

    Create send.js

---
+++ api/send.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+
+  if (req.method !== "POST")
+    return res.status(405).json({msg:"Method not allowed"});
+
+  const {title, body, topic} = req.body;
+
+  const response = await fetch(
+    "https://fcm.googleapis.com/v1/projects/YOUR_PROJECT_ID/messages:send",
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + process.env.FCM_TOKEN,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message:{
+          topic: topic || "all",
+          notification:{
+            title: title,
+            body: body
+          }
+        }
+      })
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
